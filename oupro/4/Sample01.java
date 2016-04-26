a9c51299ed4db4c1befaf224a06b6bfdd82829b7
nekonoha <tan.fantazma@gmail.com>
2016-04-26 12:16:29 +0900

    a

--- oupro/4/Sample01.java
+++ oupro/4/Sample01.java
@@ -2,11 +2,24 @@ class Sample01{
 	public static void main(String[] args){
 		String istr = "Tokyo University of Technology";
 
-		char c = istr.CharAt(2);
+		char c = istr.charAt(2);
 		System.out.println("char at position 2 is " + c);
-		
-		String ostr = "";
-		System.out.println();
+
+		String ostr = istr.toLowerCase();
+		System.out.println(ostr);
+
+		System.out.println("index of o is " + istr.indexOf('o'));
+
+		System.out.println("index of o is " + istr.lastIndexOf('o'));
+
+		String str = new String("University").toLowerCase();
+
+		if(str.equals(new String("university"))){
+			System.out.println("onaji");
+		}else{
+			System.out.println("tigau");
+		}
+
 
 
 
