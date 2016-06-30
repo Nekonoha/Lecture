3dc60fa1828ea1657eb90c18a6e829faaecbfafa
nekonoha <nekonoha>
2016-06-30 14:37:52 +0900

    turai

--- oupro/11/Kadai05.java
+++ oupro/11/Kadai05.java
@@ -5,32 +5,22 @@
  */
 package kg11;
 
-import java.util.TreeSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Kadai05 {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String str = "An eye for an eye, a tooth for a tooth.";
-        String[] text = str.toLowerCase().replaceAll("[\\W]+"," ").split(" ");
-        
-        Map<String,Integer> map = new TreeMap<>();
-        
-        
-        
-        for(String s: text){
-            map.put(s,0);
+        String[] text = str.toLowerCase().replaceAll("[\\W]+", " ").split(" ");
+        Map<String, Integer> map = new TreeMap<>();
+
+        for (String s : text) {
+            map.putIfAbsent(s, 0);
+            map.put(s, map.get(s) + 1);
         }
-        
-        for(String s: text){
-            map.put(s,map.get(s) + 1);
-        }
-              
-        
+
         Set<Map.Entry<String, Integer>> entries = map.entrySet();
-        
-        for(Map.Entry<String,Integer> entry : entries){
+        for (Map.Entry<String, Integer> entry : entries) {
             String key = entry.getKey();
             int value = entry.getValue();
             System.out.println(key + "\t : " + value);
